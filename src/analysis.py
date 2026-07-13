d940cc81737bfc25331e9c94d1bdbd8dc25b6b58
kieriam <kieriamm@gmail.com>
2026-07-13 16:31:03 +0200

    analysis progres

--- src/analysis.py
+++ src/analysis.py
@@ -3,10 +3,10 @@ import numpy as np
 def calculate_stats(df):
 
     return {
-        "Średnia": round(df["value"].mean(),2),
-        "Minimum": round(df["value"].min(),2),
-        "Maksimum": round(df["value"].max(),2),
-        "Mediana": round(df["value"].median(),2),
-        "Odchylenie": round(np.std(df["value"]),2)
+        "Średnia": round(df["Wartość"].mean(),2),
+        "Minimum": round(df["Wartość"].min(),2),
+        "Maksimum": round(df["Wartość"].max(),2),
+        "Mediana": round(df["Wartość"].median(),2),
+        "Odchylenie": round(np.std(df["Wartość"]),2)
     }
     
