f6b6f7d4d94263a92d3cf5a72722b25dbb353dea
kieriam <kieriamm@gmail.com>
2026-07-13 19:26:52 +0200

    analysis upgrade

--- src/analysis.py
+++ src/analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def calculate_stats(df):
 
@@ -9,4 +10,32 @@ def calculate_stats(df):
         "Mediana": round(df["Wartość"].median(),2),
         "Odchylenie": round(np.std(df["Wartość"]),2)
     }
+
+def compare_stations(df1, station1_name,df2, station2_name, pollutant):
+
+    df1["Data"] = pd.to_datetime(df1["Data"])
+    df2["Data"] = pd.to_datetime(df2["Data"])
+
+    plt.figure(figsize = (12,6))
+
+    plt.plot(
+        df1["Data"],
+        df1["Wartość"],
+        label = station1_name,
+        marker="o"
+    )
+
+    plt.tittle(f"Porównanie stacji - {pollutant}")
+
+    plt.xlabel("Data")
+    plt.ylabel("µg/m³")
+
+    plt.legend()
+    plt.grid()
+
+    plt.xticks(rotation=45)
+
+    plt.tight_layout()
+
+    plt.show()
     
